a0dbf8693da36407cb84e664899810a34cf8866e
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2022-02-20 00:03:30 +0100

    specified readme format

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords=["api", "wrapper", "generic", "rest api"],
     name="generic-api-wrapper",
