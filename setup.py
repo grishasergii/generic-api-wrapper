2f380baf2ba0592adc9407e68b2e4c68973ed0b7
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2022-02-19 19:55:17 +0100

    fixed issues with building the package

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@
 
 from setuptools import find_packages, setup
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
-
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
 
 requirements = [
     "requests",
@@ -39,7 +36,7 @@ setup(
     description="Generic API wrapper",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     include_package_data=True,
     keywords="wrapy",
     name="wrapy",
